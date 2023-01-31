fe90a0def1fa7cb5acc1c057c4f52a04c22be33d
Karolina Hamerszmidt <karolina.hamerszmidt@shell.com>
2023-01-31 13:43:56 +0100

    wip

--- task9.js
+++ task9.js
@@ -1,10 +1,18 @@
 
-const content = `[D]    
+const content = `    [D]    
 [N] [C]    
 [Z] [M] [P]
- 1   2   3 `
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2 `
 
 
- console.log({content})
+console.log({content})
 
 
+const myArray = content.split("/n").map((line) => line.split(" "));
+
+console.log(myArray)
